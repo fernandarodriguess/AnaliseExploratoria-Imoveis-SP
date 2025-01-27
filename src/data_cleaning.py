d921e4eff4da285bc3908b0c5617f32dd3b9ec94
Fernanda Rodrigues Santana <fernandarsantana28@gmail.com>
2025-01-27 17:37:42 +0000

    Não remover bairros únicos

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -2,11 +2,6 @@ import pandas as pd
 import numpy as np
 
 def clean_data(df):
-    # Remover linhas com bairros únicos
-    column_bairro = 'district'
-    district_counts = df[column_bairro].value_counts()
-    relevant_district = district_counts[district_counts > 1].index
-    df = df[df[column_bairro].isin(relevant_district)]
 
     # Remover outliers baseado na área e no preço de aluguel
     df = df[(df['area'] > 20) & (df['area'] < 400) & (df['rent'] > 549) & (df['rent'] < 15001)]
