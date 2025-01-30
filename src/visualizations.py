3e751cd8de594a64503aeb82b9eb7440129e2f46
Fernanda Rodrigues Santana <fernandarsantana28@gmail.com>
2025-01-30 02:19:25 +0000

    Gráficos

--- src/visualizations.py
+++ src/visualizations.py
@@ -9,7 +9,7 @@ def load_data(file_path):
 def plot_area_vs_rent(df):
     """Gráfico de disposição entre área e aluguel."""
     sns.scatterplot(data=df, x='area', y='rent')
-    plt.title('Área vs Aluguel')
+    plt.title('Relação entre Área vs Aluguel')
     plt.xlabel('Área (m²)')
     plt.ylabel('Aluguel (R$)')
     plt.show()
@@ -17,7 +17,7 @@ def plot_area_vs_rent(df):
 def plot_bedrooms_vs_rent(df):
     """"Gráfico de dispersão entre quantidade de quartos e aluguel."""
     sns.scatterplot(data=df,  x='bedrooms', y='rent')
-    plt.title('Quartos vs Aluguel')
+    plt.title('Relação entre quantidade de Quartos vs Aluguel')
     plt.xlabel('Quantidade de quartos')
     plt.ylabel('Aluguel (R$)')
     plt.show()
@@ -25,18 +25,17 @@ def plot_bedrooms_vs_rent(df):
 def plot_garage_vs_rent(df):
     """Gráfico de dispersão entre vagas na gareagem e aluguel."""
     sns.scatterplot(data=df, x='garage', y='rent')
-    plt.title('Garagem vs Aluguel')
+    plt.title('Relação entre vagas de Garagem vs Aluguel')
     plt.xlabel('Quantidade de vagas (garagem)')
     plt.ylabel('Aluguel (R$)')
     plt.show()
 
 def plot_area_vs_rent_by_zone(df):
-    """Gráfico de dispersão entre área e aluguel, separando por zona."""
+    """Gráfico de dispersão entre área e aluguel, separado por zona."""
     # Identifica as colunas que representam as zonas
     zone_columns = [col for col in df.columns if col.startswith('zone')]
-    
-    plt.figure(figsize=(10, 6))
 
+    plt.figure(figsize=(6, 4))
     # Loop que adiciona os pontos de cada zona no scatter plot
     for zone in zone_columns:
         zone_name = zone.replace('zone_', '')
@@ -56,21 +55,43 @@ def plot_area_vs_rent_by_zone(df):
     plt.show()
 
 def plot_avg_rent_by_zone(df):
-    """Gráfico de barras da média do aluguel por zona."""
-    zone_columns = [col for col in df.columns if col.startswith('zone_')]
+    """Gráfico de barras da média do aluguel por zona."""    
+    zone_columns = [col for col in df.columns if col.startswith('zone_')]  # Corrigido para usar 'df'
 
-    # Calcula a média do aluguel
     zone_rent_mean = {}
     for zone in zone_columns:
-        zone_rent_mean[zone.replace('zone_', '')] = df.loc[df[zone]]
+        # Multiplica o valor da coluna dummy pelo aluguel e calcula a média
+        zone_rent_mean[zone.replace('zone_', '')] = df.loc[df[zone] == 1, 'rent'].mean()
 
-    plt.figure(figsize=(10, 6))
-    plt.bar(zone_rent_mean.keys(), zone_rent_mean.values(), color='skyblue')
+    # Criar o gráfico de barras
+    plt.figure(figsize=(6, 4))
+    plt.bar(zone_rent_mean.keys(), zone_rent_mean.values(), color='skyblue') 
     plt.title('Média dos Aluguéis por Zona')
     plt.xlabel('Zona')
     plt.ylabel('Média do Aluguel (R$)')
     plt.xticks(rotation=45)
     plt.show()
+
+
+def plot_avg_rent_by_property_type(df):
+    """Gráfico de barras da média do aluguel por tipo de moradia."""
+    # Identificar as colunas que representam os tipos de imóveis
+    type_columns = [col for col in df.columns if col.startswith('type_')]
+
+    # Calcular a média do aluguel para cada tipo
+    property_rent_mean = {}
+    for property_type in type_columns:
+        property_rent_mean[property_type.replace('type_', '')] = df.loc[df[property_type] == 1, 'rent'].mean()
+
+    # Criar o gráfico de barras
+    plt.figure(figsize=(6, 4))
+    plt.bar(property_rent_mean.keys(), property_rent_mean.values(), color='skyblue')
+    plt.title('Média dos Aluguéis por Tipo de Moradia')
+    plt.xlabel('Tipo de Moradia')
+    plt.ylabel('Média do Aluguel (R$)')
+    plt.xticks(rotation=45)
+    plt.show()
+
 
 if __name__ == "__main__":
     df_clean = load_data('data/processed/dataset_imoveis_sp_clean.csv')
@@ -80,6 +101,5 @@ if __name__ == "__main__":
     plot_garage_vs_rent(df_clean)
     plot_area_vs_rent_by_zone(df_clean)
     plot_avg_rent_by_zone(df_clean)
-
-
+    plot_avg_rent_by_property_type(df_clean)
 
